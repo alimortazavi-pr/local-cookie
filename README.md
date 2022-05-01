306c0bf06729cbb80ff7d71fb7ad89134423af48
Ali Mortazavi <mortazavimaneshali@gmail.com>
2022-05-02 00:44:19 +0430

    V 0.0.2

---
+++ README.md
@@ -0,0 +1,1 @@
+You can use cookie like local-storage
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Set a Cookie
+export function setCookie(cookieName: string, cookieValue: any) {
+  document.cookie = cookieName + "=" + cookieValue + "; ";
+}
+
+// Get a Cookie
+export function getCookie(cookieName: string) {
+  const name = cookieName + "=";
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArr = cDecoded.split("; ");
+  let res;
+  cArr.forEach((val) => {
+    if (val.indexOf(name) === 0) res = val.substring(name.length);
+  });
+  return res;
+}
